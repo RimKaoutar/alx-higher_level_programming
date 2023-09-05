4c0289e02e16d4327e7d30e8c1332745dfbdc120
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-09-06 00:37:38 +0100

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-skip = 2
+skip = 1
+keep_skipping = 0
 for i in range(1, 100):
     if i % 10 == 0:
-        i += skip
         skip += 1
+        keep_skipping = skip - 1
+        continue
+    if keep_skipping:
+        keep_skipping -= 1
+        continue
     if i == 89:
         print("{}".format(i))
         break
