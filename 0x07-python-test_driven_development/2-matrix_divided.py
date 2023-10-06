ba1c5db60849fa1d2d180318421ef107adfcbd13
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-10-06 03:42:42 +0100

    Divide a matrix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,25 +13,28 @@ def matrix_divided(matrix, div):
         TypeError: if matrix is not a list of lists of int or float.
         TypeError: if each row of matrix is not of same size.
         TypeError: if div is neither an int nor float.
-        ZeroDivisionError: if div is zero.
+        ZeroDivisionError: if div is zero
 
     Returns:
         a new matrix with elements rounded to 2 decimal places.
     """
-    if (not isinstance(matrix, list) or matrix == [] or
-            not all(isinstance(row, list) for row in matrix) or
-            not all((isinstance(ele, int) or isinstance(ele, float))
-                    for ele in [num for row in matrix for num in row])):
-        raise TypeError("matrix must be a matrix (list of lists) of "
-                        "integers/floats")
-
-    if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("Each row of the matrix must have the same size")
-
-    if not isinstance(div, int) and not isinstance(div, float):
+    if not isinstance(div, (float, int)):
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
+    if not isinstance(matrix, list)\
+        or len(matrix) == 0 or not matrix[0]\
+        or all(len(row) == 0 for row in matrix)\
+        or not all(isinstance(ele, (int, float))
+                   for row in matrix for ele in row):
+        raise TypeError("matrix must be a matrix (list of lists)" +
+                        " of integers/floats")
+
+    row_len = len(matrix[0])
+    if any(len(row) != row_len for row in matrix):
+        raise TypeError("Each row of the matrix must have the same size")
+
+    mat = [[round(ele / div, 2) for ele in row] for row in matrix]
+    return mat
