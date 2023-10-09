3a55b41eadc12ae4b60d9921027f24d097aae060
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-10-09 22:57:05 +0100

    Integer validator

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Define a class BaseGeometry based on 6-base_geometry.py"""
+
+
+class BaseGeometry:
+    """class BaseGeometry"""
+    def area(self):
+        """Public instance method that raises an Exception
+        with the message area() is not implemented.
+
+        Raises:
+            Exception: when area is not implemented.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Public instance method hat validates value.
+
+        Args:
+            name (str): name of the variable.
+            value (int): value to assign to name.
+
+        Raises:
+            TypeError: when the value is not integer.
+            ValueError: when value is less than 0.
+        """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
