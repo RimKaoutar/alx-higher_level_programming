7876c5d49b11c3dd101efc04fae1fb22bd0d0065
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-10-10 00:45:34 +0100

    Full rectangle

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -17,3 +17,12 @@ class Rectangle(BaseGeometry):
 
         self.__width = width
         self.__height = height
+
+    def area(self):
+        """
+        calculates the area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
