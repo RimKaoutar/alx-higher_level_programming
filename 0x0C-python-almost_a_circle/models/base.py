b7bafb68bb1ba849c9b51f4f33f965e8d45e9fb2
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-10-20 15:01:46 +0100

    Base class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,7 +8,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-         """Initialize a new instance of Base.
+        """Initialize a new instance of Base.
 
         Args:
             id (int, optional): The unique identifier
