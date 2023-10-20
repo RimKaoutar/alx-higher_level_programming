f145c1eab9a5d218792a163a9479b425aeafaf22
RimKaoutar <rim.kaoutar-etu@etu.univh2c.ma>
2023-10-20 22:59:56 +0100

    JSON string to dictionary

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -68,5 +68,5 @@ class Base:
             list: JSON string representation json_string
         """
         if json_string is None or len(json_string) == 0:
-            return "[]"
+            return []
         return json.loads(json_string)
